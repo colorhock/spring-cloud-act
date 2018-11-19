018944961e1eac8216362a44894d8d6ffd3da513
daizhiyue <954374298@qq.com>
2018-11-19 21:39:06 +0800

    Initial commit

---
+++ microservice-discovery-eruka-ha/mvn-start.cmd
@@ -0,0 +1,3 @@
+java -jar eureka-ha-0.0.1-SNAPSHOT.jar --spring.propiles.active=peer1
+java -jar eureka-ha-0.0.1-SNAPSHOT.jar --spring.propiles.active=peer2
+java -jar eureka-ha-0.0.1-SNAPSHOT.jar --spring.propiles.active=peer3
---
+++ microservice-discovery-eruka-ha/src/main/java/com/example/microservicediscoveryeruka/ErukaApplication.java
@@ -0,0 +1,14 @@
+package com.example.microservicediscoveryeruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ErukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErukaApplication.class, args);
+	}
+}
